110c3f3be303e839f6c8310c3716150eb2aa6d8c
Adrian Kremer <a.kr3mer@gmail.com>
2020-03-28 17:08:06 +0100

    feat(emotion): added react emotion example including ssr (#3)
    * feat(emotion): added react emotion example including ssr

---
+++ css-frameworks/react-emotion-ssr/webpack.config.js
@@ -0,0 +1,19 @@
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const TsConfigWebpackPlugin = require("ts-config-webpack-plugin");
+
+module.exports = {
+  context: __dirname,
+  mode: "production",
+  plugins: [
+    // Generate a base html file and injects all generated css and js files
+    new HtmlWebpackPlugin({
+      template: "./src/ssr.tsx"
+    }),
+    // Multi threading typescript loader configuration with caching for .ts and .tsx files
+    // see https://github.com/namics/webpack-config-plugins/tree/master/packages/ts-config-webpack-plugin/config
+    new TsConfigWebpackPlugin()
+  ],
+  resolve: {
+    aliasFields: ["module"]
+  }
+};
